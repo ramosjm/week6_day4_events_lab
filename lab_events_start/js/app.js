d81c76c60c834e99bd626414e32578dbf218905f
ramosjm <jjramos_2004@hotmail.com>
2019-02-21 15:50:35 +0000

    final commit.

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
+  const deleteButton = document.createElement('input');
+  deleteButton.type = 'button';
+  deleteButton.value = 'Delete';
+  const h1Prepend = document.querySelector('#new-item-form');
+  h1Prepend.appendChild(deleteButton);
+
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit',()=>{
     console.dir(event.target);
