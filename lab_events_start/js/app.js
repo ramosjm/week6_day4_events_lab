182dee5064ed491fdbeccbc67e6bbeef536d1ef6
ramosjm <jjramos_2004@hotmail.com>
2019-02-21 15:22:15 +0000

    items will now move to reading list.

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
-})
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit',()=>{
+    console.dir(event.target);
+    event.preventDefault();
+    const title = event.target.title.value;
+    const author = event.target.author.value;
+    const category = event.target.category.value;
+    const newReadingItem = document.createElement('p');
+    newReadingItem.textContent = `Title : ${title} Author: ${author} Category: ${category}`;
+    const readingList = document.querySelector('#reading-list');
+    readingList.appendChild(newReadingItem);
+
+  });
+
+});
